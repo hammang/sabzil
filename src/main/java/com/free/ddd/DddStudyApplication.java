1eb233c681444fa4857fe9ff1a0f9016ddd0a96d
Jinho Kim <tiger.ohgirafferes@gmail.com>
2022-10-20 16:05:58 +0900

    hello world 추가 #4
    hello world를 서버 구동 시 콘솔 화면에 출력되도록 구현해 두었습니다.

--- src/main/java/com/free/ddd/DddStudyApplication.java
+++ src/main/java/com/free/ddd/DddStudyApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DddStudyApplication {
 
     public static void main(String[] args) {
+
+        System.out.println("hello world");
         SpringApplication.run(DddStudyApplication.class, args);
     }
 
